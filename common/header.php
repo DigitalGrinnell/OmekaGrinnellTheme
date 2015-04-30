7d0e354a504f2bb501ed277002b77b9202df9a30
Mike Conner <connerms@grinnell.edu>
2015-04-30 14:26:14 -0500

    troubleshoot analytics

--- common/header.php
+++ common/header.php
@@ -39,13 +39,21 @@
     <?php #queue_js_file('vendor/masonry'); ?>
     <?php #queue_js_file('vendor/imagesloaded'); ?>
     <?php queue_js_file('vendor/bootstrap'); ?>
-    <?php queue_js_file('wmi'); ?>
     <?php echo head_js(); ?>
 
 </head>
 <?php echo body_tag(array('id' => @$bodyid, 'class' => @$bodyclass)); ?>
     <?php fire_plugin_hook('public_body', array('view'=>$this)); ?>
-    <?php include_once("analytics.php") ?>
+    <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-6552526-3', 'auto');
+  ga('send', 'pageview');
+
+</script>
     <div id="wrap">
         <header>
             <div id="site-title">
