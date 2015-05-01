91c854c3e9fc11af9aa1cac1857888ee8a36038b
Mike Conner <connerms@grinnell.edu>
2015-05-01 10:59:27 -0500

    change 'featured item' to 'featured instrument' on landing page

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 <?php if (get_theme_option('Display Featured Item') !== '0'): ?>
 <!-- Featured Item -->
 <div id="featured-item">
-    <h2><?php echo __('Featured Item'); ?></h2>
+    <h2><?php echo __('Featured Instrument'); ?></h2>
     <?php echo random_featured_items(1); ?>
 </div><!--end featured-item-->
 <?php endif; ?>
